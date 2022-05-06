24a0db0f622abd04975ca986335e39717d886775
Zachary Imbriaco <zachary@imbriaco.com>
2022-05-06 11:22:37 -0400

    Tests passing.

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -12,12 +12,15 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
+
+    result = @hash[:results]
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
@@ -26,6 +29,7 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
 
+    result = @hash[:results].filter { |result| result[:subsection] == "Politics" }
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
